b24a10819ded9bd176c998c9fa3c24e00a2dd077
Khushi5612 <itzkhush2024@gmail.com>
2025-11-23 17:25:28 -0500

    added flavour cards

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ fetch("./data/flavours.json")
   .then(response => response.json())
   .then(data => {
     const container = document.querySelector(".flavours");
+    data.forEach(item => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      container.appendChild(card);
+    });
     })
   .catch(error => {
     console.error("Error loading JSON:", error);
